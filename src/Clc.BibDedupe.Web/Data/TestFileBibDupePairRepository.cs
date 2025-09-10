18d10744922e23ae9e9a617694005d0c9f816bb2
mtfields <mfields@clcohio.org>
2025-09-10 11:36:00 -0400

    Allow test pair repository to return multiple pairs

--- src/Clc.BibDedupe.Web/Data/TestFileBibDupePairRepository.cs
+++ src/Clc.BibDedupe.Web/Data/TestFileBibDupePairRepository.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using System.Threading;
 using Clc.BibDedupe.Web.Models;
@@ -8,6 +9,12 @@ namespace Clc.BibDedupe.Web.Data;
 public class TestFileBibDupePairRepository : IBibDupePairRepository
 {
     private int _nextId;
+    private readonly int _pairCount;
+
+    public TestFileBibDupePairRepository(int pairCount = 10)
+    {
+        _pairCount = pairCount;
+    }
 
     private BibDupePair CreatePair() => new()
     {
@@ -17,11 +24,25 @@ public class TestFileBibDupePairRepository : IBibDupePairRepository
         RightBibId = Interlocked.Increment(ref _nextId)
     };
 
+    private IEnumerable<BibDupePair> GeneratePairs(int count)
+        => Enumerable.Range(0, count).Select(_ => CreatePair());
+
     public Task<IEnumerable<BibDupePair>> GetAsync()
-        => Task.FromResult<IEnumerable<BibDupePair>>(new[] { CreatePair() });
+        => Task.FromResult(GeneratePairs(_pairCount));
 
     public Task<(IEnumerable<BibDupePair> Items, int TotalCount)> GetPagedAsync(int page, int pageSize)
-        => Task.FromResult(((IEnumerable<BibDupePair>)new[] { CreatePair() }, 1));
+    {
+        var total = _pairCount;
+        var skip = (page - 1) * pageSize;
+        if (skip >= total)
+        {
+            return Task.FromResult(((IEnumerable<BibDupePair>)Enumerable.Empty<BibDupePair>(), total));
+        }
+
+        var take = Math.Min(pageSize, total - skip);
+        var items = GeneratePairs(take);
+        return Task.FromResult((items, total));
+    }
 
     public Task MergeAsync(int keepBibId, int deleteBibId, string userEmail) => Task.CompletedTask;
 
