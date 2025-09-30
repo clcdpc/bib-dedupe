b95319351f56a7a81e4c36e316632814b28e6897
mtfields <mfields@clcohio.org>
2025-09-30 17:22:45 -0400

    Use strongly typed XRequestedWith header

--- src/Clc.BibDedupe.Web/Extensions/HttpRequestExtensions.cs
+++ src/Clc.BibDedupe.Web/Extensions/HttpRequestExtensions.cs
@@ -7,13 +7,13 @@ namespace Clc.BibDedupe.Web.Extensions;
 public static class HttpRequestExtensions
 {
     private const string XmlHttpRequest = "XMLHttpRequest";
-    private const string RequestedWithHeader = "X-Requested-With";
 
     public static bool IsAjaxRequest(this HttpRequest request)
     {
         ArgumentNullException.ThrowIfNull(request);
 
-        return request.Headers.TryGetValue(RequestedWithHeader, out var headerValue)
-            && headerValue.Any(value => string.Equals(value, XmlHttpRequest, StringComparison.OrdinalIgnoreCase));
+        var headerValue = request.Headers.XRequestedWith;
+
+        return headerValue.Any(value => string.Equals(value, XmlHttpRequest, StringComparison.OrdinalIgnoreCase));
     }
 }
