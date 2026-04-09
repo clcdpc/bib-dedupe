32374e770ca8e3cf84287143d29ab23facf4d0a1
mtfields <mfields@clcohio.org>
2026-04-09 12:12:42 -0400

    Change UserClaims from view to table

--- sql/BibDedupe_Update.sql
+++ sql/BibDedupe_Update.sql
@@ -639,11 +639,14 @@ IF OBJECT_ID('BibDedupe.UserClaims', 'V') IS NOT NULL
     DROP VIEW BibDedupe.UserClaims;
 GO
 
-CREATE VIEW BibDedupe.UserClaims
-AS
--- Grant application access by returning at least one row with Claim = 'Access'.
--- Assign additional roles (for example 'Administrator') by returning more rows for the same user.
-SELECT TOP (0)
-    CAST(NULL AS NVARCHAR(256)) AS UserEmail,
-    CAST(NULL AS NVARCHAR(256)) AS Claim;
+IF OBJECT_ID('BibDedupe.UserClaims', 'U') IS NOT NULL
+    DROP TABLE BibDedupe.UserClaims;
+GO
+
+CREATE TABLE BibDedupe.UserClaims
+(
+    UserEmail NVARCHAR(256) NOT NULL,
+    Claim NVARCHAR(256) NOT NULL,
+    CONSTRAINT PK_UserClaims PRIMARY KEY (UserEmail, Claim)
+);
 GO
